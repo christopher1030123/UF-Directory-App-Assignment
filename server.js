ff7f7515c4a20564aa272c16ed72297487de33b8
christopher1030123 <christopher1030123@gmail.com>
2018-09-04 05:48:54 -0400

    Assignment1

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+  var pathname = url.parse(request.url, true).pathname;
+  if(request.method === 'GET' && /\/listings/.test(pathname)){
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.write(JSON.stringify(listingData));
+    response.end();
+  }
+  else{
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Bad gateway error");
+    response.end();
+	}
 
   /*
     Your request handler should send listingData in the JSON format if a GET request 
@@ -19,8 +30,20 @@ var requestHandler = function(request, response) {
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
+  if (err){
+        console.log(err);
+    } else {
+    listingData= JSON.parse(data); //now it an object
+   // listingData = JSON.stringify(obj); //convert it back to json
+    //fs.writeFile('listings.json', listingData, 'utf8', callback);
+	http.createServer(function (request, response) {
+		requestHandler(request,response);
+	}).listen(8080);
+	console.log('Server started');
+	}
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
 });
+
