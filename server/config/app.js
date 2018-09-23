d7eac11fdf10008126e4d0cc7e6011c9571d48e7
christopher1030123 <30283570+christopher1030123@users.noreply.github.com>
2018-09-23 15:47:40 -0400

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -6,6 +6,6 @@ module.exports.start = function() {
   var app = express.init();
     const PORT = process.env.PORT || config.port;
   app.listen(PORT, () => {
-    console.log('App listening on port',${ PORT });
+    console.log('App listening on port',PORT);
   });
 };
